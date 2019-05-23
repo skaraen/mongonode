85fd635a5e22b47fd39415b4b424031f4030cb95
Karaen <skaraen@gmail.com>
2019-05-23 05:09:52 -0700

    Integration with Dialogflow

---
+++ schema/subwayOrderSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+let subwayOrderSchema = mongoose.Schema({
+    bread : String,
+    patty : String,
+    fillers : Array,
+    sauces : Array,
+    size : String,
+    number : Number
+})
+
+module.exports = subwayOrderSchema;
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const subwayOrderSchema = require('./schema/subwayOrderSchema')
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+const url = 'mongodb://localhost/subway';
+
+mongoose.connect(url,{useNewUrlParser: true})
+                    .then(res => console.log("Connected to DB"))
+                    .catch(err => console.log(err));
+
+let subwayOrderModel = mongoose.model('subway-order',subwayOrderSchema);
+
+app.post('/order',function(req,res){
+    console.log(req.body);
+    let bread, patty, fillers,sauces,size,number;
+
+    bread=req.body.queryResult.parameters.SubwayBreads;
+    patty=req.body.queryResult.parameters.SubwayPatties;
+    fillers=req.body.queryResult.parameters.SubwayFillers;
+    sauces=req.body.queryResult.parameters.SubwaySauces;
+    size=req.body.queryResult.parameters.SubwaySizes;
+    number=req.body.queryResult.parameters.number;
+
+    let response = {
+        bread : bread,
+        patty : patty,
+        fillers : fillers,
+        sauces : sauces,
+        size : size,
+        number : number
+    }
+
+    let sauceList="";
+    sauces.forEach(function(sauce){
+        sauceList+=sauceList+" ";
+    })
+
+    let fillerList="";
+    fillers.forEach(function(filler){
+        fillerList+=filler+" ";
+    })
+
+    let resposeText="Here's your order : "+number+" "+size+" "+bread+" "+patty+" subs loaded with "+fillerList+"and spiced up with "+sauceList;
+
+    subwayOrderModel.create(response,function(err,docs){
+        if(err) throw err;
+        console.log("Order placed!");
+        res.send("Order placed!");
+    })
+
+    console.log(resposeText);
+
+    return res.json({
+        "fulfillmentText" : resposeText,
+        "fulfillmentMessages" : [{text:{text:[resposeText]}}],
+        "source" : ""
+    })
+});
+
+app.get('/find',function(req,res){
+    let query=req.query.id;
+    console.log(query);
+    subwayOrderModel.findOne({id:query},function(err,docs){
+        if(err) throw err;
+        res.send(docs);
+        console.log(docs);
+    })
+})
+
+var server=app.listen(3000,function(){
+    var host=server.address().address;
+    var port=server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port);
+});
